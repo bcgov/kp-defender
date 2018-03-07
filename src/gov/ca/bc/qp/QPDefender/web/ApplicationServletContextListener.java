878b9b11407f4117b37cd1e765ef9e7f871c450b
Chris.Ditcher <chris.ditcher@qp.gov.bc.ca>
2018-03-07 13:28:04 -0800

    Updated logging to use proper log location for catalina

--- src/gov/ca/bc/qp/QPDefender/web/ApplicationServletContextListener.java
+++ src/gov/ca/bc/qp/QPDefender/web/ApplicationServletContextListener.java
@@ -13,6 +13,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 /**
@@ -22,10 +23,13 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class ApplicationServletContextListener implements ServletContextListener
 {
+	
+	static final Logger log = Logger.getLogger(ApplicationServletContextListener.class);
+	
     public void contextInitialized(ServletContextEvent event) 
     { 
 	ServletContext ctx = event.getServletContext();
-
+	log.info("Initializing QPDefender Application");
 	String prefix =  ctx.getRealPath("/");     
 	String file = "WEB-INF"+System.getProperty("file.separator")+"classes"+System.getProperty("file.separator")+"log4j.properties";
 
